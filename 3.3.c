3b4e85aa1039b07504b94e72b736587762f00541
vieira-andre <andre.vieira@outlook.com>
2019-05-05 08:52:16 -0300

    Add resolution to 3.3

---
+++ 3.3.c
@@ -0,0 +1,25 @@
+/* To write a program that receives the number of seconds elapsed in a given 
+ * event and prints the time in the hours-minutes-seconds format, where both 
+ * hours and minutes are exhibited with two digits and leading zeros, and 
+ * seconds are exhibited with two decimal places, also with leading zeros.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int secondsInHour = 3600, secondsInMinute = 60;
+    int seconds, hours, minutes;
+
+    printf("Number of seconds: ");
+    scanf("%d", &seconds);
+
+    hours = seconds / secondsInHour;
+    seconds -= hours * secondsInHour;
+    minutes = seconds / secondsInMinute;
+    seconds -= minutes * secondsInMinute;
+
+    printf("%02dh:%02dm:%05.2fs", hours, minutes, (float)seconds);
+    
+    getchar();
+    return 0;
+}
