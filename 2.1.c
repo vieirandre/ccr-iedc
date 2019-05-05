8167bbce355bdf1867645797aaa94ce195b800ff
vieira-andre <andre.vieira@outlook.com>
2019-05-05 07:16:57 -0300

    Add resolution to 2.1

---
+++ 2.1.c
@@ -0,0 +1,20 @@
+/* To write a program that, given the value of the radius, 
+calculates and prints the volume of the corresponding sphere. */
+
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main(void) {
+    double radius, sphereVolume;
+
+    printf("Radius value: ");
+    scanf("%lf", &radius); getchar();
+
+    sphereVolume = 4.0 / 3.0 * M_PI * pow(radius, 3);
+
+    printf("%f", sphereVolume);
+
+    getchar();
+    return 0;
+}
