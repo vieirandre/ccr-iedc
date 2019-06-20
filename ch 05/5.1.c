480eeddb72b91b47a298641ec09070547eb0250b
vieira-andre <andre.vieira@outlook.com>
2019-06-20 02:15:54 -0300

    Add solution to 5.1

---
+++ ch 05/5.1.c
@@ -0,0 +1,36 @@
+/*
+ * To implement a iterative function to calculate the greatest common divisor of two positive integer numbers, using the the Euclidean algorithm.
+ */
+
+#include <stdio.h>
+
+int mdc(int x, int y);
+
+int main(void) {
+    int x, y, result;
+
+    printf("Insert x: ");
+    scanf("%d", &x); getchar();
+
+    printf("Insert y: ");
+    scanf("%d", &y); getchar();
+
+    result = mdc(x, y);
+
+    printf("The greatest common divisor of %d and %d is: %d\n", x, y, result);
+    
+    getchar();
+    return 0;
+}
+
+int mdc(int x, int y) {
+    int r;
+
+    while (y != 0) {
+        r = x % y;
+        x = y;
+        y = r;
+    }
+
+    return x;
+}
